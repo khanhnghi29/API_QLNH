9e00645294f3b1e5584572b026c1995fed00463a
KhanhNghi <khanhnghi29xunu@gmail.com>
2023-03-28 16:20:45 +0700

    Add project files.

---
+++ API_QLNH/Controllers/ThucDonController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Reflection.Metadata.Ecma335;
+using API_QLNH.Model;
+namespace API_QLNH.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ThucDonController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public ThucDonController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = "Select MaThucDon, TenThucDon from ThucDon";
+            DataTable table = new DataTable();
+            String sqlDataSource = _configuration.GetConnectionString("QLNH");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using(SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+        [HttpPost]
+        public JsonResult Post(ThucDon thucDon)
+        {
+            string query = @"Insert into ThucDon values(N'" + thucDon.TenThucDon + "')";
+            DataTable table = new DataTable();
+            String sqlDataSource = _configuration.GetConnectionString("QLNH");
+            SqlDataReader myReader;
+            using(SqlConnection myCon = new SqlConnection( sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();   
+                    table.Load(myReader);   
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Thêm món ăn thành công");
+
+        }
+
+        
+    }
+}
