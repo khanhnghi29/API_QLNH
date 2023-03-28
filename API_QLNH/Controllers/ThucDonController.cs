d42b35f34d1e6b390ac4c7a0fbdb1ed44427ee1f
KhanhNghi <khanhnghi29xunu@gmail.com>
2023-03-28 16:20:45 +0700

    Add project files.

---
+++ API_QLNH/Controllers/ThucDonController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Reflection.Metadata.Ecma335;
+using API_QLNH.Model;
+namespace API_QLNH.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ThucDonController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public ThucDonController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = "Select MaThucDon, TenThucDon from ThucDon";
+            DataTable table = new DataTable();
+            String sqlDataSource = _configuration.GetConnectionString("QLNH");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using(SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+        [HttpPost]
+        public JsonResult Post(ThucDon thucDon)
+        {
+            string query = @"Insert into ThucDon values(N'" + thucDon.TenThucDon + "')";
+            DataTable table = new DataTable();
+            String sqlDataSource = _configuration.GetConnectionString("QLNH");
+            SqlDataReader myReader;
+            using(SqlConnection myCon = new SqlConnection( sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();   
+                    table.Load(myReader);   
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Thêm món ăn thành công");
+
+        }
+        [HttpPut]
+        public JsonResult Put(ThucDon thucDon)
+        {
+            string query = @"Update ThucDon set
+                TenThucDon = N'" + thucDon.TenThucDon + "' " +
+                " where MaThucDon = " + thucDon.MaThucDon;
+            DataTable table = new DataTable();
+            String sqlDataSource = _configuration.GetConnectionString("QLNH");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Cập nhật thành công!");
+        }
+
+        [HttpDelete("{ma}")]
+        public JsonResult Delete(int ma)
+        {
+            string query = @"Delete From ThucDon " +
+                " where MaThucDon = " + ma;
+            DataTable table = new DataTable();
+            String sqlDataSource = _configuration.GetConnectionString("QLNH");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Xóa bỏ thành công!");
+        }
+
+    }
+}
